8846199cc9c5f7219c6b672aa05b20109cf70a0b
Rubens Diego <rdsdo2011@gmail.com>
2021-02-03 22:42:08 -0300

    Criando Menu

--- src/component/Menu/index.tsx
+++ src/component/Menu/index.tsx
@@ -1,11 +1,27 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
 import './styles.css'
+import {ImMenu} from "react-icons/im";
 
 const Menu: React.FC = () => {
     return(
-        <>
-            
-        </>
+        <div>
+            <input type='checkbox' id='check' ></input>
+            <label htmlFor="check">
+<span><ImMenu className='icon' /></span>
+            </label>
+            <nav>
+                <ul>
+                    <li><Link to="/Inicio">Home</Link></li>
+                    <li><Link to="/cadastro-usuario">Usuário</Link></li>
+                    <li><Link to='/cadastro-dieta'>CadastroDieta</Link></li>
+                    <li><Link to='/cadastro-exercicio'>CadastroExercicio</Link></li>
+                    <li><Link to='/cadastro-treino'>CadastroTreino</Link></li>
+                    <li><Link to='/cadastro-academia'>Academia</Link></li>
+                </ul>
+            </nav>
+        </div>
+
     )
 }
 
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Menu from "../../component/Menu";
 
 
 
@@ -7,7 +8,7 @@ import React from 'react'
 
 return(
      <div>
-      ola
+         <Menu/>
      </div>
 )
 }
