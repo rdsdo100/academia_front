5577e642d53ef532ef2cd3b5afe9111af8f433dc
Rubens Diego <rdsdo2011@gmail.com>
2021-02-09 13:36:34 -0300

    Estudando redux

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,18 @@
 import React from 'react';
 import Route from './routes/routes'
 import GlobalStyles from './styles/GlobalStyles';
+import {Provider} from "react-redux";
+import store from "./store/store";
 
+//const store = createStore(reducer, initialState);
 
 function App() {
   return (
       <div>
-
+          <Provider store={store}>
               <GlobalStyles />
               <Route/>
-
+          </Provider>
       </div>
 
 
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,8 +1,19 @@
 import React from 'react'
 import Menu from "../../component/Menu";
 import Layout from "../../component/Layout";
+import {useDispatch, useSelector} from "react-redux";
 
  const Home: React.FC = () => {
+
+     const dispatch = useDispatch()
+
+
+/*// TS infers type: (state: RootState) => boolean
+     const selectIsOn = (state: RootState) => state.isOn
+
+// TS infers `isOn` is boolean
+     const isOn = useSelector(selectIsOn)*/
+
 
 return(
     <Layout>
