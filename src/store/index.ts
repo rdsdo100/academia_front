013f0c857f2aab8cef3dae0137203fe22194a89e
Rubens Diego <rdsdo2011@gmail.com>
2021-02-11 16:30:11 -0300

    Criando redux so o começo

---
+++ src/store/index.ts
@@ -0,0 +1,16 @@
+import { createStore, Store } from 'redux';
+
+import { RepositoriesState } from './ducks/repositories/types';
+
+import rootReducer from './ducks/rootReducer';
+
+
+export interface ApplicationState {
+  repositories: RepositoriesState
+}
+
+
+const store: Store<ApplicationState> = createStore(rootReducer);
+
+
+export default store;
