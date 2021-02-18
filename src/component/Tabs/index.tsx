c34c6c7566d69309d669a023792565197530f48e
Rubens Diego <rdsdo2011@gmail.com>
2021-02-18 00:10:38 -0300

    adicionado novos menus

--- src/component/Tabs/index.tsx
+++ src/component/Tabs/index.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import './styles.css'
-import Input from "../Input";
+
 
 
 const Tabs: React.FC = () => {
+
+
     return(
         <div>
         <nav className='nav_tabs'>
@@ -12,22 +14,21 @@ const Tabs: React.FC = () => {
                     <input type='radio' name='tabs' className='rd_tabs' id='tab1' />
                     <label className='labelTabs' htmlFor="tab1">Tab1</label>
                     <div className="content">
-                        <Input type='text'> <label>Nome</label></Input>
-                        <Input type='text'><label>Sobrenome</label></Input>
+
                     </div>
                 </li>
                 <li>
                     <input type='radio' name='tabs' className='rd_tabs' id='tab2' />
                     <label className='labelTabs' htmlFor="tab2">Tab2</label>
                     <div className="content">
-                        <p>Deigo</p>
+
                     </div>
                 </li>
                 <li>
                     <input type='radio' name='tabs' className='rd_tabs' id='tab3' />
                     <label className='labelTabs' htmlFor="tab3">Tab3</label>
                     <div className="content">
-                        <p>teste</p>
+
                     </div>
                 </li>
                 <li>
--- src/pages/Exercicio/CadastroExercicio/index.tsx
+++ src/pages/Exercicio/CadastroExercicio/index.tsx
@@ -6,7 +6,9 @@ import Tabs from "../../../component/Tabs";
 const CadastroExercicio: React.FC = () => {
     return(
         <div>
-        <Tabs></Tabs>
+        <Tabs>
+
+        </Tabs>
         </div>
     )
 }
