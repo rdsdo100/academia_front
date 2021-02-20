6d0a53143f134f18587513f0a7ac43c43f02c1e6
Rubens Diego <rdsdo2011@gmail.com>
2021-02-20 13:02:25 -0300

    Refatorando Codigo

--- src/pages/ErrorPage/index.tsx
+++ src/pages/ErrorPage/index.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import ok from '../../assets/Error.jpg'
-import './styles.css'
+import {IMG , H1} from './styles'
 
 const ErrorPage: React.FC = () => {
     return(
         
 <div>
-    <img src={ok}  />
-<h1> &lt; ErrorPage	&gt;</h1>
+    <IMG src={ok}  />
+<H1> &lt; Error Page	&gt;</H1>
 </div>
 
     )
