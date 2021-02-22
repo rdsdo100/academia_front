dd8af4b10a5b82bd5af50077aa5b4d45ffb2fc09
Rubens Diego <rdsdo2011@gmail.com>
2021-02-22 13:54:54 -0300

    refatorando codigo

---
+++ src/component/input/Toggle/index.tsx
@@ -0,0 +1,16 @@
+import React, { InputHTMLAttributes } from 'react';
+import {LoginBoxs , UserBoxs, Input,Label} from "./styles"
+
+type IInputProps = InputHTMLAttributes<HTMLInputElement>;
+
+const Toggle: React.FC<IInputProps> = ({  children, ...rest }) =>  (
+
+        <LoginBoxs>
+            <UserBoxs>
+                <Input required {...rest} />
+               <Label>{children}</Label>
+            </UserBoxs>
+        </LoginBoxs>
+
+    );
+export default Toggle;
